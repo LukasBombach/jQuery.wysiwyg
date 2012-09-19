4f064e79ca746c486ff6ba1f49a4fc99f2b3db31
Lukas Bombach <luke@Lukas-Bombachs-MacBook-Pro.local>
2012-09-19 19:02:33 +0200

    first commit so exited xoxoxo

---
+++ javascript/jquery.editable/plugins/cmd.js
@@ -0,0 +1,29 @@
+(function( $ ){
+
+
+  //console.log($.fn["editable"].prototype.Editor.prototype);
+
+  $.fn["editable"].prototype.Editor.prototype.cmd = function( command, value, ui ) {
+  
+    // 1 Parameter String -> execCommand(cmd)
+    // 2 Parameter String -> execCommand(cmd, val)
+    // 3 Parameter String -> execCommand(cmd, val, ui)
+    // { 'name': function, ...} -> neue commands hinzufügen
+
+    if( !this.hasFocus() ) {
+      console.log("cmd '"+command+"' not ecexuted. This Editor is not focussed.");
+      return this;
+    }
+
+    command = command || '';
+    value   = value   || null;
+    ui      = ui      || false;
+    
+    window.document.execCommand(command, ui, value);
+
+    return this;
+
+  };
+  
+
+})( jQuery );
